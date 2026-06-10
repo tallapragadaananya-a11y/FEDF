9b2aa0cbffc878c92a7a9322b6d8e99cebc693cf
tallapragadaananya-a11y <tallapragada.ananya@gmail.com>
2026-06-10 09:19:23 +0530

    Update Register.jsx

--- Register.jsx
+++ Register.jsx
@@ -1,28 +1,96 @@
-import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 import "../styles/Register.css";
-
+import Navbar from "../components/Navbar";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 function Register() {
+
+  const navigate = useNavigate();
+
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  const handleRegister = (e) => {
+
+    e.preventDefault();
+
+    const users =
+      JSON.parse(localStorage.getItem("users")) || [];
+
+    const exists =
+      users.find(
+        user => user.email === email
+      );
+
+    if(exists){
+
+      alert("Email already exists");
+      return;
+    }
+
+    const newUser = {
+
+      id: Date.now(),
+
+      username: name,
+
+      email,
+
+      password
+    };
+
+    users.push(newUser);
+
+    localStorage.setItem(
+      "users",
+      JSON.stringify(users)
+    );
+
+    alert("Registration Successful");
+
+    navigate("/login");
+  };
+  
   return (
     <>
       <Navbar />
 
       <div className="register-container">
-        <form className="register-form">
-          <h2>Create Account</h2>
+
+        <form className="register-form"  onSubmit={handleRegister}>
+
+          <h2>Create Account ✨</h2>
+
+          <p>
+            Join ShopWish and organize
+            your shopping experience.
+          </p>
 
           <input
             type="text"
-            placeholder="Full Name"
+            placeholder="Full Name" value={name}
+            onChange={(e)=>
+            setName(e.target.value)
+           }
           />
 
           <input
             type="email"
-            placeholder="Email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e)=>
+            setEmail(e.target.value)
+           }
           />
 
           <input
             type="password"
             placeholder="Password"
+            value={password}
+            onChange={(e)=>
+            setPassword(e.target.value)
+            }
           />
 
           <input
@@ -31,12 +99,20 @@ function Register() {
           />
 
           <button type="submit">
-            Register
+            Create Account
           </button>
+
+          <span>
+            Already have an account?
+            <Link to="/login">
+              Sign In
+            </Link>
+          </span>
+
         </form>
+
       </div>
     </>
   );
 }
-
 export default Register;
