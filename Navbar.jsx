65860a036064eeddf8dfb1e33f00e94ea80d7704
tallapragadaananya-a11y <tallapragada.ananya@gmail.com>
2026-06-10 09:17:20 +0530

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -2,25 +2,56 @@ import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 
 function Navbar() {
+
+  const user = JSON.parse(
+    localStorage.getItem("currentUser")
+  );
+
   return (
     <nav className="navbar">
-    <div className="logo">
-    ShopWish ✨
-    </div>
+
+      <div className="logo">
+        ShopWish ✨
+      </div>
 
       <ul className="nav-links">
+
         <li>
           <Link to="/">Home</Link>
         </li>
 
         <li>
-          <Link to="/login">Login</Link>
+          <Link to="/wishlist">
+            Wishlist
+          </Link>
         </li>
 
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
+        {!user ? (
+          <>
+            <li>
+              <Link to="/login">
+                Login
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/register">
+                Register
+              </Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/profile">
+                Hello, {user.username}
+              </Link>
+            </li>
+          </>
+        )}
+
       </ul>
+
     </nav>
   );
 }
