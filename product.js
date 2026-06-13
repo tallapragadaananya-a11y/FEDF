babc3d6ab5229571ca759a3389655e6798528a75
tallapragadaananya-a11y <tallapragada.ananya@gmail.com>
2026-06-13 13:44:14 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,49 @@
+const products = [
+
+{
+id:1,
+name:"Lavender Oversized Hoodie",
+price:1499,
+rating:4.6,
+website:"Myntra",
+category:"Fashion",
+image:"https://images.unsplash.com/photo-1521572163474-6864f9cf17ab",
+description:"Soft oversized hoodie perfect for everyday wear."
+},
+
+{
+id:2,
+name:"Wireless Earbuds",
+price:2999,
+rating:4.5,
+website:"Amazon",
+category:"Electronics",
+image:"https://images.unsplash.com/photo-1505740420928-5e560c06d30e",
+description:"Noise-cancelling wireless earbuds."
+},
+
+{
+id:3,
+name:"Butter Yellow Tote Bag",
+price:899,
+rating:4.8,
+website:"Littlebox",
+category:"Fashion",
+image:"https://images.unsplash.com/photo-1542291026-7eec264c27ff",
+description:"Minimal aesthetic tote bag."
+},
+
+{
+id:4,
+name:"Desk Lamp",
+price:1299,
+rating:4.4,
+website:"Flipkart",
+category:"Home",
+image:"https://images.unsplash.com/photo-1505693416388-ac5ce068fe85",
+description:"Modern study desk lamp."
+}
+
+];
+
+export default products;
